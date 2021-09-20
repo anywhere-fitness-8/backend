2a9c9d212e200f2c11ed87d81f5b530416c55c96
Dharmik Savaliya <danny@Dharmiks-MacBook-Pro.local>
2021-09-20 11:48:31 -0700

    eslint fix

--- api/server.js
+++ api/server.js
@@ -1,29 +1,29 @@
-const express = require("express");
-const helmet = require("helmet");
-const cors = require("cors");
-const usersRouter = require("./users/users-router");
-const authRouter = require("./auth/auth-router");
-const classesRouter = require("./classes/classes-router");
+const express = require('express')
+const helmet = require('helmet')
+const cors = require('cors')
+const usersRouter = require('./users/users-router')
+const authRouter = require('./auth/auth-router')
+const classesRouter = require('./classes/classes-router')
 
-const server = express();
-server.use(express.json());
-server.use(helmet());
-server.use(cors());
+const server = express()
+server.use(express.json())
+server.use(helmet())
+server.use(cors())
 
-server.use("/api/auth", authRouter);
-server.use("/api/users", usersRouter);
-server.use("/api/classes", classesRouter);
+server.use('/api/auth', authRouter)
+server.use('/api/users', usersRouter)
+server.use('/api/classes', classesRouter)
 
-server.get("/", (req, res) => {
-  res.json({ api: "up" });
-});
+server.get('/', (req, res) => {
+  res.json({ api: 'up' })
+})
 
+// eslint-disable-next-line
 server.use((err, req, res, next) => {
-  // eslint-disable-line
   res.status(err.status || 500).json({
     message: err.message,
     stack: err.stack,
-  });
-});
+  })
+})
 
-module.exports = server;
+module.exports = server
